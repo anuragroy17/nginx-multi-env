881e25f3ec474e1dbbbf06819d2feef9047df93e
Anurag Roy <anuragroy17@gmail.com>
2023-11-09 19:56:44 +0530

    add default values

--- src/assets/config.temp.js
+++ src/assets/config.temp.js
@@ -1,5 +1,5 @@
 // This file will have the mappings and envsubst will replace the config.js with the mapped environment variables
 __config = {
-  apiUrl: "${API_URL}",
-  environment: "${ENV}",
+  apiUrl: '${API_URL}' || 'local_url',
+  environment: '${ENV}' || 'local',
 };
